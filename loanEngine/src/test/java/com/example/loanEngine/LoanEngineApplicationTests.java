db70357553c9f421bdb3759ed1a73ebba2adeb22
Michał Gutowski <mgutowski2003@gmail.com>
2026-03-12 17:15:53 +0100

    Spring and React app added

---
+++ loanEngine/src/test/java/com/example/loanEngine/LoanEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
